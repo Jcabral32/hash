0996c420d8d2a154ec986847e4aa4851ade67c75
jcabral32 <jcabral18@gmail.com>
2018-03-16 12:49:52 -0400

    Added the code for the Hash Set. Need to add code for the Hash Table--> and the Hash Function.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -26,10 +26,66 @@ struct Input:Encodable,Content {
 struct Output:Encodable,Content {
     let outList: [[String]]
 }
+// Add the HashSet Code
+public struct HashSet<T: Hashable> {
+    private var dictionary = Dictionary<T, Bool>()
+    
+    public mutating func insert(element: T) {
+        dictionary[element] = true
+    }
+    
+    public mutating func remove(element: T) {
+        dictionary[element] = nil
+    }
+    
+    public func contains(element: T) -> Bool {
+        return dictionary[element] != nil
+    }
+    
+    public func allElements() -> [T] {
+        return Array(dictionary.keys)
+    }
+    
+    public var count: Int {
+        return dictionary.count
+    }
+    
+    public var isEmpty: Bool {
+        return dictionary.isEmpty
+    }
+}
 
-func hashFunction(key: String)->Int{
-    // Needs to take unique lowercase characters from a String and sum the ascii values of the lowercase letters.
-    return 0
+extension HashSet {
+    public func union(otherSet: HashSet<T>) -> HashSet<T> {
+        var combined = HashSet<T>()
+        for obj in dictionary.keys {
+            combined.insert(element: obj)
+        }
+        for obj in otherSet.dictionary.keys {
+            combined.insert(element: obj)
+        }
+        return combined
+    }
+    
+    public func intersect(otherSet: HashSet<T>) -> HashSet<T> {
+        var common = HashSet<T>()
+        for obj in dictionary.keys {
+            if otherSet.contains(element: obj) {
+                common.insert(element: obj)
+            }
+        }
+        return common
+    }
+    
+    public func difference(otherSet: HashSet<T>) -> HashSet<T> {
+        var diff = HashSet<T>()
+        for obj in dictionary.keys {
+            if !otherSet.contains(element: obj) {
+                diff.insert(element: obj)
+            }
+        }
+        return diff
+    }
 }
 
 
